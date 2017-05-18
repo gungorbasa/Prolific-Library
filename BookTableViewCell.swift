0f7ce91895be3904dcaba493add52f499c688254
Güngör <gungorbasa@gmail.com>
2017-05-18 12:33:20 -0700

    Tableviewcell label resizing issue is solved. PKHUD view is added. Test Cases are added.

---
+++ BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  The Prolific Library
+//
+//  Created by Gungor Basa on 5/18/17.
+//  Copyright © 2017 Gungor Basa. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
